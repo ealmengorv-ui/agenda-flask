cadb2cf520375c481f2169e3edb87fe8765c7e47
Eduardo Almengor <ealmengorv@miumg.edu.gt>
2026-09-05 12:40:21 -0600

    Agrega validacion de campos en backend

--- app.py
+++ app.py
@@ -8,14 +8,20 @@ agenda = []
 @app.route("/", methods=["GET", "POST"])
 def inicio():
     if request.method == "POST":
-        persona = {
-            "nombre": request.form["nombre"],
-            "apellido": request.form["apellido"],
-            "fecha_nacimiento": request.form["fecha_nacimiento"],
-            "dia_semana": request.form["dia_semana"]
-        }
+        nombre = request.form["nombre"].strip()
+        apellido = request.form["apellido"].strip()
+        fecha_nacimiento = request.form["fecha_nacimiento"]
+        dia_semana = request.form["dia_semana"]
 
-        agenda.append(persona)
+        if nombre and apellido and fecha_nacimiento and dia_semana:
+            persona = {
+                "nombre": nombre,
+                "apellido": apellido,
+                "fecha_nacimiento": fecha_nacimiento,
+                "dia_semana": dia_semana
+            }
+
+            agenda.append(persona)
 
         return redirect(url_for("inicio"))
 
